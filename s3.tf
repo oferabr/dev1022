e06ea99af057dbe22a523edc930ea5d02f35e7fc
oferabr <42511847+oferabr@users.noreply.github.com>
2023-09-10 23:27:28 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,7 +7,6 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
-  force_destroy = true
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
